c482f9a44345d5f37deea961f7165b0aaffd3435
TriGonostylops <martonbalazsi@gmail.com>
2025-07-19 11:59:37 +0200

    initial commit: backend scaffold

---
+++ backend/src/main/java/com/spotologist/SpotologistApplication.java
@@ -0,0 +1,13 @@
+package com.spotologist.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotologistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotologistApplication.class, args);
+	}
+
+}

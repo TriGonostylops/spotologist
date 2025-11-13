952237b297f7e534785d6747115e58910d22dc33
TriGonostylops <martonbalazsi@gmail.com>
2025-11-13 15:21:44 +0100

    feat: remove unused repo func

--- backend/src/main/java/com/spotologist/spots/repository/SpotRepository.java
+++ backend/src/main/java/com/spotologist/spots/repository/SpotRepository.java
@@ -3,8 +3,5 @@ package com.spotologist.spots.repository;
 import com.spotologist.spots.model.Spot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SpotRepository extends JpaRepository<Spot, Long> {
-    List<Spot> findByUserId(Long userId);
 }

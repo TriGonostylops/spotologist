5398a50156a459edda3720ddc80169f5b07fce51
TriGonostylops <martonbalazsi@gmail.com>
2025-11-14 15:56:05 +0100

    feat(#1): hello world from backend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SpotsComponent } from './spots/spots';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SpotsComponent },
+  { path: '**', redirectTo: '' }
+];

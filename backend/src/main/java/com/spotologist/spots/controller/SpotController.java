5c7034af45a93e477c9883a3a62f795c11c85e3d
TriGonostylops <martonbalazsi@gmail.com>
2025-11-17 12:02:26 +0100

    feat: hello api

--- backend/src/main/java/com/spotologist/spots/controller/SpotController.java
+++ backend/src/main/java/com/spotologist/spots/controller/SpotController.java
@@ -3,6 +3,7 @@ package com.spotologist.spots.controller;
 import com.spotologist.spots.model.Spot;
 import com.spotologist.spots.service.SpotService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,11 @@ public class SpotController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(value = "/hello", produces = "text/plain")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello from Spotologist backend!");
     }
 }

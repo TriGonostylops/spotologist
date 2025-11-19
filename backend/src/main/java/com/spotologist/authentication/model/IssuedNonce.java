cc7a035a3c77e915c43de3123c4e68578c81b761
TriGonostylops <martonbalazsi@gmail.com>
2025-11-19 14:29:52 +0100

    feat: removed unused javadoc

--- backend/src/main/java/com/spotologist/authentication/model/IssuedNonce.java
+++ backend/src/main/java/com/spotologist/authentication/model/IssuedNonce.java
@@ -1,6 +1,3 @@
 package com.spotologist.authentication.model;
 
-/**
- * Immutable value object returned when the server issues a nonce.
- */
 public record IssuedNonce(String nonce, long expiresIn) {}

a4cb6cd68d09aa8bd11738974db05b3c82c9d570
TriGonostylops <martonbalazsi@gmail.com>
2025-11-28 15:59:44 +0100

    feat: removed dummy controller-service functions

--- backend/src/main/java/com/spotologist/features/spot/service/SpotService.java
+++ backend/src/main/java/com/spotologist/features/spot/service/SpotService.java
@@ -1,11 +1,7 @@
 package com.spotologist.features.spot.service;
 
-import com.spotologist.features.spot.model.Spot;
 import com.spotologist.features.spot.repository.SpotRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class SpotService {
@@ -13,31 +9,5 @@ public class SpotService {
 
     public SpotService(SpotRepository spotRepository) {
         this.spotRepository = spotRepository;
-    }
-
-    public Spot saveSpot(Spot spot) {
-        return spotRepository.save(spot);
-    }
-
-    public List<Spot> getAllSpots() {
-        return spotRepository.findAll();
-    }
-
-    public Optional<Spot> findSpotById(Long id) {
-        return spotRepository.findById(id);
-    }
-
-    public Optional<Spot> updateSpotById(Long id, Spot updatedSpotDto) {
-        return spotRepository.findById(id).map(spot -> {
-            spot.setTitle(updatedSpotDto.getTitle());
-            spot.setDescription(updatedSpotDto.getDescription());
-            spot.setLatitude(updatedSpotDto.getLatitude());
-            spot.setLongitude(updatedSpotDto.getLongitude());
-            spot.setCity(updatedSpotDto.getCity());
-            spot.setCountry(updatedSpotDto.getCountry());
-            spot.setAddress(updatedSpotDto.getAddress());
-            spot.setPublic(updatedSpotDto.isPublic());
-            return spotRepository.save(spot);
-        });
     }
 }

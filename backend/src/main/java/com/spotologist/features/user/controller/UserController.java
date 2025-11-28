f9dbd4e7c454abd80ae0ddf5896e4e92bbe5c9de
TriGonostylops <martonbalazsi@gmail.com>
2025-11-28 09:48:44 +0100

    feat: fixed token flow

--- backend/src/main/java/com/spotologist/features/user/controller/UserController.java
+++ backend/src/main/java/com/spotologist/features/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.spotologist.features.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/name")
     public ResponseEntity<UserDto> getUserNameById(@AuthenticationPrincipal Jwt jwt){
         return ResponseEntity.ok().body(userService.getUserNameById(jwt.getSubject()));
